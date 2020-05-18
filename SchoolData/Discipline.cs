b2fa6efff977a655a4a158f7b9d3d58bdfa65e56
DanielGospodinov <63073767+DanielGospodinov@users.noreply.github.com>
2020-05-19 01:15:39 +0300

    Add files via upload

---
+++ SchoolData/Discipline.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolData
+{
+    class Discipline
+    {
+        private string name;
+        private uint numbLectures;
+        private uint numbExercises;
+
+        public Discipline(string name, uint numbLectures, uint numbExrcicses)
+        {
+            this.name = name;
+            this.numbLectures = numbLectures;
+            this.numbExercises = numbExrcicses;
+        }
+
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+        }
+        public uint NumbExercises
+        {
+            set { this.numbExercises = value; }
+            get { return this.numbExercises; }
+        }
+        public uint NumbLectures
+        {
+            set { this.numbLectures = value; }
+            get { return this.numbLectures; }
+        }
+
+        public override string ToString()
+        {
+            string result = String.Format($"{this.Name} (Exrecices : {this.NumbExercises}, Lections : {this.NumbLectures}) ");
+            return result;
+        }
+    }
+}
---
+++ SchoolData/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolData
+{
+    public class Student : Human
+    {
+        private int classNumber;
+
+        public Student(string firstName, string lastName, int classNumber) : base(firstName, lastName)
+        {
+            this.classNumber = classNumber;
+        }
+
+        public int ClassNumber
+        {
+            get { return this.classNumber; }
+            set { this.classNumber = value; }
+        }
+        public override string ToString()
+        {
+            string result = String.Empty;
+            result = String.Format($"{this.classNumber}. {this.FirtstName} {this.LastName} ");
+            return result;
+        }
+    }
+}
